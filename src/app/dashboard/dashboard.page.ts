7f0cab9d30631f055742445cee0c63aacba879e2
Yinzy00 <yari.marien@telenet.be>
2021-12-08 22:59:12 +0100

    Added device switch functionality

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -17,27 +17,53 @@ export class DashboardPage implements OnInit {
     private activatedRoute: ActivatedRoute,
     private hubitat: HubitatApiService,
     private dashboardService: DashboardService,
-    private loadingService : LoadingService
+    private loadingService: LoadingService
   ) {
-   }
+  }
   async ngOnInit() {
     let id: string = this.activatedRoute.snapshot.paramMap.get('id');
     let loading = await this.loadingService.presentLoadingWithOptions('Loading dashboard');
-    this.dashboardService.getDashboardById(id).then(data=>{
+    this.dashboardService.getDashboardById(id).then(async data => {
       this.dashboard = data;
       this.title = this.dashboard.Title;
-      this.devices = this.dashboard.Devices;
+      for (let i = 0; i < this.dashboard.Devices.length; i++) {
+        const d = this.dashboard.Devices[i];
+        await this.hubitat.getDeviceById(d.id).then(data => {
+          let _d = new OnOffDevice();
+          Object.assign(_d, data);
+          // console.log(_d);
+          this.devices.push(_d);
+          this.devices[i][i] = (_d.isOn() ? true : false)
+        });
+      }
     });
     loading.dismiss();
   }
   private dashboard: Dashboard;
   private title: string;
-  private devices: OnOffDevice[];
+  private devices: OnOffDevice[] = [];
 
-  private switchDevice(device){
-  console.log(device);
+  private async switchDevice(index: string) {
+    let deviceId = this.devices[index].id;
+    //Update device state
+    let device = new OnOffDevice();
+    Object.assign(device, await this.hubitat.getDeviceById(deviceId));
+
+    console.log(device.isOn());
+
+    // //Check
+    console.log(this.devices);
+    if (!this.devices[index][index]) {
+      console.log("Turning off");
+      device.SendCommand(this.hubitat.getCommandUrl(device.id, "off"));
+    }
+    else {
+      console.log("Turning on");
+      device.SendCommand(this.hubitat.getCommandUrl(device.id, "on"));
+    }
+
   }
-  private Edit(){
+  private Edit() {
     console.log("got to edit dashboard view");
   }
 }

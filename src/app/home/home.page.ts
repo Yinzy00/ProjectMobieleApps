a82fdbf7fa7259815b8110e83bb0ab801ec2c5be
Yinzy00 <yari.marien@telenet.be>
2021-12-08 01:23:47 +0100

    added list of dashboards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Dashboard } from 'src/types/dashboard';
 import { Device } from 'src/types/device';
 import { OnOffDevice } from 'src/types/deviceTypes/onOffDevice';
 import { AuthenticationService } from '../services/authentication.service';
+import { DatabaseService } from '../services/database.service';
 import { HubitatApiService } from '../services/hubitat-api.service';
 import { CreateComponent } from './create/create.component';
 
@@ -12,24 +14,31 @@ import { CreateComponent } from './create/create.component';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-public test:string = 'test';
-  constructor(public authService: AuthenticationService, public route: Router, public modalController: ModalController, public hubitatApiService: HubitatApiService) {}
+export class HomePage implements OnInit {
+  public test: string = 'test';
+  constructor(
+    public authService: AuthenticationService,
+    public route: Router,
+    public modalController: ModalController,
+    public hubitatApiService: HubitatApiService,
+    public dbService: DatabaseService
+  ) { }
   async ngOnInit(): Promise<void> {
-    
+    await this.loadDashboards();
   }
 
+  async loadDashboards() {
+    this.dashboards = await this.dbService.getDashboards();
+  }
+  dashboards: Dashboard[] = [];
 
-  devices: Array<Device>;
-
-filter(array:Device[]):Array<Device>{
-  return array.filter(x=>x.id=="101");
-}
-
-  public async showCreateModal():Promise<void>{
+  public async showCreateModal(): Promise<void> {
     const modal = await this.modalController.create({
       component: CreateComponent
     });
     await modal.present();
+    await modal.onDidDismiss().then(async value => {
+      await this.loadDashboards();
+    });
   }
 }

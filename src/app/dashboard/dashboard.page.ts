cf603c7a198c15c2046e7cb61f6e7e649f4e25ed
Yinzy00 <yari.marien@telenet.be>
2021-12-08 02:34:14 +0100

    Added loading dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Dashboard } from 'src/types/dashboard';
+import { OnOffDevice } from 'src/types/deviceTypes/onOffDevice';
+import { DashboardService } from '../services/dashboard.service';
+import { HubitatApiService } from '../services/hubitat-api.service';
+import { LoadingService } from '../services/loading.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private hubitat: HubitatApiService,
+    private dashboardService: DashboardService,
+    private loadingService : LoadingService
+  ) {
+   }
+  async ngOnInit() {
+    let id: string = this.activatedRoute.snapshot.paramMap.get('id');
+    let loading = await this.loadingService.presentLoadingWithOptions('Loading dashboard');
+    this.dashboardService.getDashboardById(id).then(data=>{
+      this.dashboard = data;
+      this.title = this.dashboard.Title;
+      this.devices = this.dashboard.Devices;
+    });
+    loading.dismiss();
   }
+  private dashboard: Dashboard;
+  private title: string;
+  private devices: OnOffDevice[];
 
+  private switchDevice(device){
+  console.log(device);
+  }
+  private Edit(){
+    console.log("got to edit dashboard view");
+  }
 }

ea3d898e70a00fba326dfb2e8ccc0261a145183d
Yinzy00 <yari.marien@telenet.be>
2021-12-13 14:40:36 +0100

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,19 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'yinzy.mysmarthomedashboard',
-  appName: 'MySmarthomeDashboard',
+  
+  appName: 'mysmarthomedashboard',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    // Onderstaande lijn genereert een linting fout
+    // Hier is niets aan te doen, behalve deze te negeren.
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com'],
+    }
+  },
 };
 
 export default config;
